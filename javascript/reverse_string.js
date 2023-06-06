4606fb6090dc8ff300bf40503bdfbd957b2d597e
Romil <romil.m.kothari@gmail.com>
2023-06-06 17:15:01 -0400

    tests completed

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,4 +1,9 @@
 function reverseString(str) {
+  newString=""
+  for(let i = str.length -1; i >= 0; i--){
+        newString = newString + str[i]
+  }
+  return newString
   // type your code here
 }
 
